623361dd4336c68079c1b577761101b80423a9ad
inaburat <inaburat@gmail.com>
2016-11-20 18:38:05 +0300

    Контрольные правки
    1. Исправлена ширина для контейнера-центровщика.
    2. Убраны ссылки у изображений карточек товаров, на странице каталога.
    3. Заданы альтернативные шрифты для всех соответствующих элементов.
    4. Исправлена ширина контейнера для карты.
    5. Подключение CSS файлов выполнено единообразно.
    6. Скрипты подключены согласно требованиям.
    7. Исправлен ховер для активных элементов на странице каталога.
    8. Исправлены отступы для карточек каталога, на случай добавления
    контента.
    9. Убраны 2 <br> в блоке с аддрессом и добавлен 1 <span> для телефонного
    номера.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var link = document.querySelector(".js-hidden-form");
+var popup = document.querySelector(".hidden-form");
+var close = document.querySelector(".hidden-form-close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("hidden-form-show")
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("hidden-form-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("hidden-form-show")) {
+      popup.classList.remove("hidden-form-show");
+    }
+  }
+});
